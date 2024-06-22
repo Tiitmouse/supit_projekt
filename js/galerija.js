0a25b7b26085c8886a711ff0a1c8e05cde23cdc8
Tiitmouse <lkovacic380@gmail.com>
2024-06-22 16:46:33 +0200

    fixed news contact modal and galery

--- js/galerija.js
+++ js/galerija.js
@@ -1,12 +1,12 @@
 
 // Open the Modal
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
+  document.getElementById("gModal").style.display = "block";
 }
 
 // Close the Modal
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+  document.getElementById("gModal").style.display = "none";
 }
 
 let slideIndex = 1;
